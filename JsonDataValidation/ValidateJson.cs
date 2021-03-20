56206fffa291c8620c48571fe5edce009fb683c2
Ed Fisk <Ed.Fisk@arup.com>
2021-03-20 20:06:49 +0000

    Logic and tests added

---
+++ JsonDataValidation/ValidateJson.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Arup.Compute.DotNetSdk;
+using Arup.Compute.DotNetSdk.Enums;
+using Arup.Compute.DotNetSdk.Attributes;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace JsonDataValidation
+{
+    public class ValidateJson
+    {
+        public static void Main() { }
+
+        [Calculation("Json validator", "Validate a json input against a json schema", "ed.fisk@arup.com", LevelOfReview.Complete)]
+        [Output("Valid?", "Is the json valid against the schema provided", "", "")]
+        public static ArupComputeResult ValidateJsonInput(
+            [Input("Json","A json input as a string","","")]
+            string input_json,
+            [Input("JsonSchema", "A json schema input as a string", "", "")]
+            string input_schema
+        )
+        {
+            try
+            {
+                // Convert schema string to LINQ schema
+                JSchema schema = JSchema.Parse(input_schema);
+
+                // Creates a JSON object
+                JObject json = JObject.Parse(input_json);
+
+                // Validates the object against the schema
+                IList<ValidationError> errorMessages;
+
+                bool isValid = json.IsValid(schema, out errorMessages);
+
+                string isValidStr = Convert.ToString(isValid);
+
+                ArupComputeResultItem acResult = new ArupComputeResultItem();
+                acResult.Value = isValidStr;
+
+                ArupComputeResult result = new ArupComputeResult();
+                result.ArupComputeResultItems = new List<ArupComputeResultItem>();
+                result.ArupComputeResultItems.Add(acResult);
+                return result;
+            }
+            catch
+            {
+                ArupComputeResult result = new ArupComputeResult();
+                result.Errors = new List<string>();
+                result.Errors.Add("The input Json or Schema was in the incorrect format");
+                return result;
+            }
+        }
+    }
+}
---
+++ JsonDataValidationTests/ValidateJson.cs
@@ -0,0 +1,83 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using JsonDataValidation;
+using Arup.Compute.DotNetSdk;
+using System.IO;
+
+namespace JsonDataValidationTests
+{
+    [TestClass]
+    public class ValidateJsonTest
+    {
+        [TestMethod]
+        public void TestMethod_AGSI_Valid_Both()
+        {
+            string schema_fp = @"../../../TestData/AGSI/AGSi_JSONSchema_v-0-5-0_Valid.json";
+            string json_fp = @"../../../TestData/AGSI/DC2-test_in_agsi-sample_Valid.json";
+
+            string json = File.ReadAllText(json_fp);
+            string schema = File.ReadAllText(schema_fp);
+
+            string Result_expected = "True";
+
+            ArupComputeResult comparison = ValidateJson.ValidateJsonInput(json, schema);
+            Assert.AreEqual(Result_expected, comparison.ArupComputeResultItems[0].Value);
+        }
+        [TestMethod]
+        public void TestMethod_AGSI_Valid_Json()
+        {
+            string schema_fp = @"../../../TestData/AGSI/AGSi_JSONSchema_v-0-5-0_Fail.json";
+            string json_fp = @"../../../TestData/AGSI/DC2-test_in_agsi-sample_Valid.json";
+
+            string json = File.ReadAllText(json_fp);
+            string schema = File.ReadAllText(schema_fp);
+
+            string Result_expected = "False";
+
+            ArupComputeResult comparison = ValidateJson.ValidateJsonInput(json, schema);
+            Assert.AreEqual(Result_expected, comparison.ArupComputeResultItems[0].Value);
+        }
+        [TestMethod]
+        public void TestMethod_AGSI_Invalid_Both()
+        {
+            string schema_fp = @"../../../TestData/AGSI/AGSi_JSONSchema_v-0-5-0_Fail.json";
+            string json_fp = @"../../../TestData/AGSI/DC2-test_in_agsi-sample_Fail.json";
+
+            string json = File.ReadAllText(json_fp);
+            string schema = File.ReadAllText(schema_fp);
+
+            string Result_expected = "False";
+
+            ArupComputeResult comparison = ValidateJson.ValidateJsonInput(json, schema);
+            Assert.AreEqual(Result_expected, comparison.ArupComputeResultItems[0].Value);
+        }
+        [TestMethod]
+        public void TestMethod_AGSI_Invalid_Json()
+        {
+            string schema_fp = @"../../../TestData/AGSI/AGSi_JSONSchema_v-0-5-0_Valid.json";
+            string json_fp = @"../../../TestData/AGSI/DC2-test_in_agsi-sample_Fail.json";
+
+            string json = File.ReadAllText(json_fp);
+            string schema = File.ReadAllText(schema_fp);
+
+            string Result_expected = "False";
+
+            ArupComputeResult comparison = ValidateJson.ValidateJsonInput(json, schema);
+            Assert.AreEqual(Result_expected, comparison.ArupComputeResultItems[0].Value);
+        }
+        [TestMethod]
+        public void TestMethod_AGSI_Invalid_Format()
+        {
+            string schema_fp = @"../../../TestData/AGSI/AGSi_JSONSchema_v-0-5-0_Valid.json";
+            string json_fp = @"../../../TestData/AGSI/DC2-test_in_agsi-sample_Invalid_Format.json";
+
+            string json = File.ReadAllText(json_fp);
+            string schema = File.ReadAllText(schema_fp);
+
+            double Errors_expected = 1;
+
+            ArupComputeResult comparison = ValidateJson.ValidateJsonInput(json, schema);
+            Assert.AreEqual(Errors_expected, comparison.Errors.Count);
+        }
+    }
+}
